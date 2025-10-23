36cd5996b344efa448539b531935ba0b2dcca4c0
Perryfixbug <hieupmdkim@gmail.com>
2025-10-23 23:32:18 +0700

    add feature remember password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,27 @@ class User < ApplicationRecord
   has_secure_password
   attr_accessor :remember_token
 
+  def remember
+    @remember_token = User.new_token
+    update_column(:remember_digest, User.digest(@remember_token))
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")  # Metaprogramming lấy giá trị digest từ db
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
   # Begin class method
   class << self
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ?
               BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-      BCrypt::Password(string, cost)
+      BCrypt::Password.create(string, cost: cost)
     end
 
     def new_token
-      SecureRandom.urlsafe.base64
+      SecureRandom.urlsafe_base64
     end
   end
   # End class method
-  private
-
 end
