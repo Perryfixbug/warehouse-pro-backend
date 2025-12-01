0eeb8de3d377e1137b43d55724a9cfc304c19672
Perryfixbug <hieupmdkim@gmail.com>
2025-12-01 21:09:11 +0700

    fix: config to API controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
-class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
-  skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::API
+  # skip_before_action :verify_authenticity_token
+  include Devise::Controllers::Helpers
 
   rescue_from CanCan::AccessDenied do |exception|
     render json: { 
